2f63ce62355cbb57bba3066b54ed42a87db12a14
Claudiu Zissulescu <claziss@synopsys.com>
2020-09-30 17:48:49 +0200

    tests/tcg: ARC: Add TCG instruction definition tests
    The added tests verify basic instructions execution as well
    as more advanced features such as zero overhead loops interrupt
    system, memory management unit and memory protection unit.
    
    Signed-off-by: Claudiu Zissulescu <claziss@synopsys.com>
    Signed-off-by: Cupertino Miranda <cmiranda@synopsys.com>

--- tests/tcg/configure.sh
+++ tests/tcg/configure.sh
@@ -47,6 +47,7 @@ fi
 : ${cross_cc_aarch64_be="$cross_cc_aarch64"}
 : ${cross_cc_cflags_aarch64_be="-mbig-endian"}
 : ${cross_cc_alpha="alpha-linux-gnu-gcc"}
+: ${cross_cc_arc="arc-elf32-gcc"}
 : ${cross_cc_arm="arm-linux-gnueabihf-gcc"}
 : ${cross_cc_cflags_armeb="-mbig-endian"}
 : ${cross_cc_hexagon="hexagon-unknown-linux-musl-clang"}
