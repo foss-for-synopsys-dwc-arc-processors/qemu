16fe75dffb0846fc599dc102e310a5aa657f350c
Cupertino Miranda <cmiranda@synopsys.com>
2021-04-06 13:16:06 +0100

    Usermode support for both ARC v2 and v3.
    Signed-off-by: Cupertino Miranda <cmiranda@synopsys.com>
    Signed-off-by: Vladimir Isaev <isaev@synopsys.com>

--- linux-user/elfload.c
+++ linux-user/elfload.c
@@ -1507,6 +1507,43 @@ static inline void init_thread(struct target_pt_regs *regs,
 
 #endif /* TARGET_HEXAGON */
 
+#ifdef TARGET_ARCV2
+
+#define ELF_START_MMAP 0x20000000
+#define ELF_ARCH  EM_ARC_COMPACT2
+
+#define ELF_CLASS ELFCLASS32
+
+static inline void init_thread(struct target_pt_regs *regs,
+                               struct image_info *infop)
+{
+    regs->sepc = infop->entry;
+    regs->sp = infop->start_stack;
+}
+
+#define ELF_EXEC_PAGESIZE 8192
+
+#endif /* TARGET_ARCV2 */
+
+#ifdef TARGET_ARCV3
+
+#define ELF_START_MMAP 0x20000000
+#define ELF_ARCH  EM_ARC_COMPACT3_64
+
+#define ELF_CLASS ELFCLASS64
+
+static inline void init_thread(struct target_pt_regs *regs,
+                               struct image_info *infop)
+{
+    regs->sepc = infop->entry;
+    regs->sp = infop->start_stack;
+}
+
+#define ELF_EXEC_PAGESIZE 4096
+
+#endif /* TARGET_ARCV3 */
+
+
 #ifndef ELF_PLATFORM
 #define ELF_PLATFORM (NULL)
 #endif
