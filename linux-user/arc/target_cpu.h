01475c278736cb6880b3e91892086ce09fd13ac0
Vineet Gupta <vgupta@synopsys.com>
2021-04-21 14:52:03 -0700

    ARC: usermode: clone fixes
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- linux-user/arc/target_cpu.h
+++ linux-user/arc/target_cpu.h
@@ -8,7 +8,8 @@ static inline void cpu_clone_regs_child(CPUARCState *env, target_ulong newsp,
         CPU_SP(env) = newsp;
     }
 
-    //env->gpr[xA0] = 0;
+    // clone return 0 in child
+    env->r[0] = 0;
 }
 
 static inline void cpu_clone_regs_parent(CPUARCState *env, unsigned flags)
@@ -17,8 +18,11 @@ static inline void cpu_clone_regs_parent(CPUARCState *env, unsigned flags)
 
 static inline void cpu_set_tls(CPUARCState *env, target_ulong newtls)
 {
-  // TODO
-    //env->gpr[xTP] = newtls;
+#if defined(TARGET_ARCv2)
+    env->r[25] = newtls;
+#else
+    env->r[30] = newtls;
+#endif
 }
 
 static inline abi_ulong get_sp_from_cpustate(CPUARCState *env)
