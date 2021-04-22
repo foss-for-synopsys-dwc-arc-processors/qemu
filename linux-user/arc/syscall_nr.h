1ddc025e275dec3a9e53a87707ec2b2c021b5522
Cupertino Miranda <cmiranda@synopsys.com>
2021-04-22 08:52:44 +0100

    Minor fixes
    Conflicts:
    	linux-user/arc/signal.c

--- linux-user/arc/syscall_nr.h
+++ linux-user/arc/syscall_nr.h
@@ -6,9 +6,9 @@
 #ifndef LINUX_USER_ARC_SYSCALL_NR_H
 #define LINUX_USER_ARC_SYSCALL_NR_H
 
-#ifdef TARGET_ARCV2
+#if defined(TARGET_ARCV2)
 # include "syscall32_nr.h"
-#elif TARGET_ARCV3
+#elif defined(TARGET_ARCV3)
 # include "syscall64_nr.h"
 #else
 # error "This should never happen !!!!"
