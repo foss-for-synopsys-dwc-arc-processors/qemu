7a8e2e8c5f8099f7b918c7930ab6712f98929922
Vladimir Isaev <isaev@synopsys.com>
2021-08-04 21:11:08 +0300

    linux-user: Fix target misprint in copy tls

--- linux-user/arc/target_cpu.h
+++ linux-user/arc/target_cpu.h
@@ -18,10 +18,12 @@ static inline void cpu_clone_regs_parent(CPUARCState *env, unsigned flags)
 
 static inline void cpu_set_tls(CPUARCState *env, target_ulong newtls)
 {
-#if defined(TARGET_ARCv2)
+#if defined(TARGET_ARCV2)
     env->r[25] = newtls;
-#else
+#elif defined(TARGET_ARCV3)
     env->r[30] = newtls;
+#else
+    #error "TARGET macro not defined!"
 #endif
 }
 
