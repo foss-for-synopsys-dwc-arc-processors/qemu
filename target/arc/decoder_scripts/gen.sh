9ea842a7cb91eee74f4b41213cff3101652c66e7
Nuno Cardoso <cardoso@debianc.home>
2022-04-28 10:15:13 +0100

    Integrated decoder scripts into QEMU build system.
    This implied converting all the ruby scripts to python and to make meson
    call the python scripts.
    Generated files are now only generated in the build directories.
    Input of the scripts are the same decoder tables used in binutils.

--- target/arc/decoder_scripts/gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat decoder_originals/arc64-tbl.h decoder_originals/arc-tbl.h > /tmp/opcodes_original.full
-ruby decoder_scripts/opcodes_process.rb < /tmp/opcodes_original.full | cat > opcodes.def
-ruby decoder_scripts/decode_tests.rb
