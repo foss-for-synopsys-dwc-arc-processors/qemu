f26d30af1f7bfdca0767060b2bc89d8831e524fc
BrunoASMauricio <brunoasmauricio@gmail.com>
2022-11-24 12:15:55 +0000

    Added dmacwh implementation

--- target/arc/semfunc-v3.c
+++ target/arc/semfunc-v3.c
@@ -14452,3 +14452,17 @@ arc_gen_QMACHU(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
 
     return DISAS_NEXT;
 }
+
+int
+arc_gen_DMACWH(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
+{
+    ARC_GEN_SEMFUNC_INIT();
+
+    arc_gen_dmacwh_base_i64(ctx, a, b, c, cpu64_acc, true, \
+                            tcg_gen_sextract_i64, \
+                            arc_gen_add_signed_overflow_i64);
+
+    ARC_GEN_SEMFUNC_DEINIT();
+
+    return DISAS_NEXT;
+}
