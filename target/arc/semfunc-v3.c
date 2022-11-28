7601ab706079ccc89ba9e4c7fe1a85d4051a45c3
BrunoASMauricio <brunoasmauricio@gmail.com>
2022-11-28 09:18:10 +0000

    Added qmpyh implementation

--- target/arc/semfunc-v3.c
+++ target/arc/semfunc-v3.c
@@ -14522,3 +14522,17 @@ arc_gen_DMPYHU(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
 
     return DISAS_NEXT;
 }
+
+int
+arc_gen_QMPYH(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
+{
+    ARC_GEN_SEMFUNC_INIT();
+
+    arc_gen_qmpyh_base_i64(ctx, a, b, c, cpu64_acc, true, \
+                            tcg_gen_sextract_i64, \
+                            arc_gen_add_signed_overflow_i64);
+
+    ARC_GEN_SEMFUNC_DEINIT();
+
+    return DISAS_NEXT;
+}
