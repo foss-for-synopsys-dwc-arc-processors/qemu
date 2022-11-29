3489d5a8390aa79603ee2c11bede15cdb2968b36
BrunoASMauricio <brunoasmauricio@gmail.com>
2022-11-29 11:16:21 +0000

    Added vmpy2hu instruction

--- target/arc/semfunc-v3.c
+++ target/arc/semfunc-v3.c
@@ -14592,3 +14592,17 @@ arc_gen_VMPY2H(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
 
     return DISAS_NEXT;
 }
+
+int
+arc_gen_VMPY2HU(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
+{
+    ARC_GEN_SEMFUNC_INIT();
+
+    arc_gen_vmpy2h_base_i64(ctx, a, b, c, cpu64_acc, false, \
+                            tcg_gen_extract_i64, \
+                            arc_gen_add_unsigned_overflow_i64);
+
+    ARC_GEN_SEMFUNC_DEINIT();
+
+    return DISAS_NEXT;
+}
