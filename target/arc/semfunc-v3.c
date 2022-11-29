c0773d76bf25ec6b1db4bed50111c7283d975553
BrunoASMauricio <brunoasmauricio@gmail.com>
2022-11-29 13:55:14 +0000

    Added dmachu implementation. Replaced old 32 bit version with standardized one, and removed decoder operand indicators.

--- target/arc/semfunc-v3.c
+++ target/arc/semfunc-v3.c
@@ -14496,6 +14496,20 @@ arc_gen_DMACH(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
 }
 
 int
+arc_gen_DMACHU(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
+{
+  ARC_GEN_SEMFUNC_INIT();
+
+  arc_gen_dmach_base_i64(ctx, a, b, c, cpu64_acc, false, \
+                         tcg_gen_extract_i64, \
+                         arc_gen_add_unsigned_overflow_i64);
+
+  ARC_GEN_SEMFUNC_DEINIT();
+
+  return DISAS_NEXT;
+}
+
+int
 arc_gen_DMPYH(DisasCtxt *ctx, TCGv a, TCGv b, TCGv c)
 {
     ARC_GEN_SEMFUNC_INIT();
