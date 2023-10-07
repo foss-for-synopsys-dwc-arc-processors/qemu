ce7d330e0cf599cdc496bad289db691c06d7c41a
Yuriy Kolerov <ykolerov@synopsys.com>
2023-10-07 09:49:32 +0400

    target/arc: Don't use a timer for RTC and enable it by default
    Using QTimer for RTC implementation is redundent and meaningless
    since it's just free-running clock.
    
    Also, enable RTC by default. If it's not available then Linux
    uses Timer 1 as fallback which is much less accurate.
    
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- target/arc/timer.h
+++ target/arc/timer.h
@@ -21,7 +21,63 @@
 #ifndef __ARC_TIMER_H__
 #define __ARC_TIMER_H__
 
+/*
+ * Timer Configuration Register bits
+ * TIMER_BUILD, AUX address: 0x75, access: R
+ *
+ *   VERSION[7:0]   - Version of timers:
+ *                        0x4 - ARCv2
+ *                        0x6 - ARCv2 with support of TD bit for timers
+ *                        0x6 - ARCv3 HS5x
+ *                        0x7 - ARCv3 HS6x
+ *   T0[8]          - Timer 0 present
+ *   T1[9]          - Timer 1 present
+ *   RTC[10]        - 64-bit RTC present
+ *   R[15:11]       - Reserved
+ *   P0[19:16]      - Indicates the interrupt priority level of Timer 0
+ *   P1[23:20]      - Indicates the interrupt priority level of Timer 1
+ *   R[31:24]       - Reserved
+ */
+
+FIELD(ARC_TIMER_BUILD, VERSION, 0, 8)
+FIELD(ARC_TIMER_BUILD, T0, 8, 1)
+FIELD(ARC_TIMER_BUILD, T1, 9, 1)
+FIELD(ARC_TIMER_BUILD, RTC, 10, 1)
+FIELD(ARC_TIMER_BUILD, P0, 16, 4)
+FIELD(ARC_TIMER_BUILD, P1, 20, 4)
+
+/*
+ * Real-Time Counter Control Register bits
+ * AUX_RTC_CTRL, AUX address: 0x103, access: RW
+ *
+ *   E[0]       - Enable: 0 - disabled counting , 1 - enabled counting
+ *   C[1]       - A value of 1 clears the AUX_RTC_LOW and AUX_RTC_HIGH registers
+ *   R[29:2]    - Reserved
+ *   A0[30]     - A bit of atomicity of reads for AUX_RTC_LOW
+ *   A1[31]     - A bit of atomicity of reads for AUX_RTC_HIGH
+ */
+
+FIELD(ARC_RTC_CTRL, ENABLE, 0, 1)
+FIELD(ARC_RTC_CTRL, CLEAR, 1, 1)
+FIELD(ARC_RTC_CTRL, A0, 30, 1)
+FIELD(ARC_RTC_CTRL, A1, 31, 1)
+
+
 void arc_initializeTIMER(ARCCPU *);
 void arc_resetTIMER(ARCCPU *);
+
+/*
+ * Return the number of global clock cycles passed for a particular CPU.
+ */
+
+uint64_t arc_get_cycles(CPUARCState *env);
+
+/*
+ * Return the number of global clock cycles passed common for all CPUs. It's
+ * used by Global Free Running Counter (GFRC) subsystem of ARConnect that is
+ * used by CPUs the global clock source.
+ */
+
+uint64_t arc_get_global_cycles(void);
 
 #endif
